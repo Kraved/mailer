820c5082b7745dc5a02f75ac57f7d3e710358f09
kraved <kraved>
2020-03-24 18:06:41 +0300

    customFindOrNew создан

--- app/Models/MailList.php
+++ app/Models/MailList.php
@@ -18,4 +18,24 @@ class MailList extends Model
     protected $table = 'mail_list';
 
     protected $guarded = [];
+
+    /**
+     * Метод ищет в таблице наличие записи по атрибутам,
+     * и добавляет новую запись в случае необнаружения.
+     * Отличие от findOr* методов в возвращаемом булевом значении.
+     * !! Возвращает тру в случае создании записи, а не успеха поиска !!
+     *
+     * @param array $attributes
+     * @return bool
+     */
+    public function customFindOrNew(array $attributes):bool
+    {
+        $find = $this->where($attributes)->get();
+        if ($find->isNotEmpty()) {
+            return false;
+        } else {
+            $this->create($attributes);
+            return true;
+        }
+    }
 }
