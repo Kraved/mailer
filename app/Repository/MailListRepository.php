67387799bb56de56eced07ff02525d47c80b651b
kraved <kraved>
2020-03-31 14:46:48 +0300

    переделан getMailToExport

--- app/Repository/MailListRepository.php
+++ app/Repository/MailListRepository.php
@@ -36,7 +36,7 @@ class MailListRepository extends CoreRepository
     {
         $data = $this->model->select('email')->get();
         $mails = $data->map(function ($item) {
-            return $item->email;
+            return str_replace("\n", '', $item->email);
         });
         return $mails;
     }
