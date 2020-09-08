f9144195ac9a47ae5737a0ef1903392a3ea42ca5
kraved <kraved>
2020-09-08 15:31:26 +0300

    рефактор web.php

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,20 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'maillist', 'name' => '123'], function () {
+Route::name('mailer.')->prefix('maillist')->group(function () {
     Route::resource('mail', 'MailListController')
-        ->names('mailer.maillist')
+        ->names('maillist')
         ->except('show');
-    Route::delete('deleteall', 'MailListController@deleteAll')->name('mailer.maillist.deleteall');
-    Route::get('/import/file', 'MailListImportController@fileImport')->name('mailer.maillist.import.file');
-    Route::get('/import/site', 'MailListImportController@siteImport')->name('mailer.maillist.import.site');
-    Route::post('/import/file/save', 'MailListImportController@fileImportHandler')->name('mailer.maillist.import.file.handler');
-    Route::post('/import/site/save', 'MailListImportController@siteImportHandler')->name('mailer.maillist.import.site.handler');
-    Route::post('/import/site/save', 'MailListImportController@siteImportHandler')->name('mailer.maillist.import.site.handler');
-    Route::get('/export', 'MailListExportController@export')->name('mailer.maillist.export');
-});
-Route::get('mailer/index', 'MailerController@index')->name('mailer.mailer.index');
-Route::post('mailer/send', 'MailerController@send')->name('mailer.mailer.send');
+    Route::delete('deleteall', 'MailListController@deleteAll')->name('maillist.deleteall');
+    Route::get('/import/file', 'MailListImportController@fileImport')->name('maillist.import.file');
+    Route::get('/import/site', 'MailListImportController@siteImport')->name('maillist.import.site');
+    Route::post('/import/file/save', 'MailListImportController@fileImportHandler')->name('maillist.import.file.handler');
+    Route::post('/import/site/save', 'MailListImportController@siteImportHandler')->name('maillist.import.site.handler');
+    Route::post('/import/site/save', 'MailListImportController@siteImportHandler')->name('maillist.import.site.handler');
+    Route::get('/export', 'MailListExportController@export')->name('maillist.export');
+});;
 
+Route::name('mailer.')->group(function () {
+    Route::get('mailer/index', 'MailerController@index')->name('mailer.index');
+    Route::post('mailer/send', 'MailerController@send')->name('mailer.send');
+});
