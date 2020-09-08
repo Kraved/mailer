1b577e2889d8b09140c8f4a78795942b7bb4b160
kraved <kraved>
2020-09-08 11:55:18 +0300

    Описание интерфейса MailListExport

--- app/Contracts/MailListExport.php
+++ app/Contracts/MailListExport.php
@@ -9,7 +9,15 @@ namespace App\Contracts;
  */
 interface MailListExport
 {
+    /**
+     * Возвращает путь временного файла
+     * @return string
+     */
     function getExportFilePath() :string;
 
+    /**
+     * Возвращает ответ сервера для возможности скачать файл
+     * @return mixed
+     */
     function getResponse();
 }
